8cc17f0af2765d4874f9140a672e055c1efabcd3
Dani-8 <daniyalw6190@gmail.com>
2026-02-25 07:18:36 +0500

    udpates

--- src/apps/basic/basicThememode.jsx
+++ src/apps/basic/basicThememode.jsx
@@ -1,28 +1,42 @@
 import { Link } from "react-router-dom";
 import { ArrowLeft } from "lucide-react";
 
+import Btn from "./components/btn"
+
 import { useState, createContext } from "react";
 
+export const ThemeContext = createContext()
 
-const themeContext = createContext()
+// ========================================================
+// ========================================================
 
 export default function BasicThemeApp() {
 
     const [theme, setTheme] = useState("light")
 
-    
-
+    const toggleTheme = () => {
+        setTheme((theme) => theme === "light" ? "dark" : "light")
+    }
 
 
     return (
-        <div className="min-h-screen p-4 md:p-12 font-sans text-slate-900 bg-[#F8FAFC]">
-            <div className="max-w-5xl mx-auto">
-                <Link to="/" className="flex items-center gap-2 text-sm text-slate-500 hover:text-indigo-600 mb-8">
-                    <ArrowLeft size={20} /> Back to Portal
-                </Link>
-                
-                
+        <ThemeContext.Provider value={{theme, toggleTheme}}>
+            <div className="min-h-screen p-4 md:p-12 font-sans text-slate-900 trasition-all duration-300"
+            style={{
+                background: theme === "light" ? "#F8FAFC" : "#222",
+                color: theme === "light" ? "#000" : "#fff",
+            }}>
+                <div className="max-w-5xl mx-auto">
+                    <Link to="/" className="flex items-center gap-2 text-sm text-slate-500 hover:text-indigo-600 mb-8">
+                        <ArrowLeft size={20} /> Back to Portal
+                    </Link>
+                    
+                    
+                    <div className="flex justify-center mt-50">
+                        <Btn />
+                    </div>
+                </div>
             </div>
-        </div>
+        </ThemeContext.Provider>
     );
 }
--- src/apps/basic/components/btn.jsx
+++ src/apps/basic/components/btn.jsx
@@ -1,10 +1,18 @@
+import { useContext } from "react";
+import { ThemeContext } from "../basicThememode";
 
 
+export default function Btn(){
+    const {theme, toggleTheme} = useContext(ThemeContext)
 
-export default function btn(){
     return(
-        <button className="">
-            
+        <button onClick={toggleTheme} 
+        style={{
+            background: theme === "light" ? "#222" : "#F8FAFC",
+            color: theme === "light" ? "#fff" : "#000",
+        }}
+        className=" p-4 rounded-xl shadow-xl cursor-pointer active:scale-90 transition-all duration-300">
+            Current Theme: {theme}
         </button>
     )
 }
